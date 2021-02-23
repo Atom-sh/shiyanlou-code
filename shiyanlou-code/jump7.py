df2c9493ba29d408bd105e1f986d9a3edff16518
Atom-sh <57655534+Atom-sh@users.noreply.github.com>
2021-02-23 16:18:02 +0800

    Update jump7.py
    中文乱码修正

--- shiyanlou-code/jump7.py
+++ shiyanlou-code/jump7.py
@@ -1,9 +1,9 @@
 #jump7.py
 
-# for??????1~100??i? Loop through a collection
+# for循环遍历集合1~100赋给i。 Loop through a collection
 for i in range(1,101):
-    str = '{}'.format(i) #?i???????
-    if i % 7 == 0 or '7' in str: # ?? i ? 7 ??? ? i ?? 7 ????
-        continue #??????????i
+    str = '{}'.format(i) #将i转化为字符串。
+    if i % 7 == 0 or '7' in str: # 如果 i 是 7 的倍数 或 i 是含 7 的数字。
+        continue #跳过本轮循环，不打印i
     else:
-        print('{}'.format(i)) #??i
+        print('{}'.format(i)) #打印i
